aa720478e974a81aedf6cae1419a919974361cda
Enan Ajmain <3nan.ajmain@gmail.com>
2023-05-06 16:04:40 +0600

    feat(main): add macros for easy benchmarking

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,16 @@
 #include <iostream>
 #include <sstream>
+#include <chrono>
 #include "decoder.h"
 
 using namespace std;
+
+#define TIMER_SETUP \
+  std::chrono::time_point<std::chrono::high_resolution_clock> __start, __stop;
+#define TIMER_START __start = std::chrono::high_resolution_clock::now();
+#define TIMER_STOP  __stop  = std::chrono::high_resolution_clock::now();
+#define TIMER_DURATION \
+  (std::chrono::duration_cast<std::chrono::microseconds>(__stop-__start).count())
 
 int
 main(int argc, char **argv)
