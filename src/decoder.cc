8dbd734e8d566346d2cc422836784412717debea
Enan Ajmain <3nan.ajmain@gmail.com>
2023-05-28 03:53:03 +0600

    refactor(decoder): prefer local variable over global
    The reason I chose to define the const hashmaps as global even though
    they're only used inside the 'decode' function was because I thought
    declaring them locally would decrease performance because it would have
    to initialize the hashmap on every function call.  But recent readings
    suggest the opposite is true.  Global static variables may cause cache
    miss whereas local variables should not.  I am still not clear which is
    preferable when, but for now I think we should go the idiomatic route:
    declare the variable in the scope it is used in.

--- src/decoder.cc
+++ src/decoder.cc
@@ -13,47 +13,23 @@ using namespace std;
 #define F3MAX   7   // 2 ^ (14-12+1)  - 1
 #define F7MAX   127 // 2 ^ (31-25+1)  - 1
 
-const unordered_map<uint8_t, format_t> format_map {
-  { 0b0110011, R },
-  { 0b0010011, I },
-  { 0b1100011, B },
-  { 0b0000011, I },
-  { 0b0100011, S },
-};
-
-const unordered_map<uint32_t, mnemonic_t> mnemonic_map {
-  /* { (f7<<10 | f3<<7 | opcode), mnemonic } */
-  /* Opcode         f3         f7 */
-  { (0b0110011 | (0x0<<7) | (0x00<<10)),  ADD },
-  { (0b0110011 | (0x0<<7) | (0x20<<10)),  SUB },
-  { (0b0110011 | (0x4<<7) | (0x00<<10)),  XOR },
-  { (0b0110011 | (0x6<<7) | (0x00<<10)),  OR },
-  { (0b0110011 | (0x7<<7) | (0x00<<10)),  AND },
-  { (0b0110011 | (0x1<<7) | (0x00<<10)),  SLL },
-  { (0b0110011 | (0x5<<7) | (0x00<<10)),  SRL },
-  { (0b0110011 | (0x5<<7) | (0x20<<10)),  SRA },
-  { (0b0010011 | (0x0<<7)),  ADDI },
-  { (0b0010011 | (0x4<<7)),  XORI },
-  { (0b0010011 | (0x6<<7)),  ORI },
-  { (0b0010011 | (0x7<<7)),  ANDI },
-  { (0b0010011 | (0x1<<7) | (0x00<<10)),  SLLI },
-  { (0b0010011 | (0x5<<7) | (0x00<<10)),  SRLI },
-  { (0b0010011 | (0x5<<7) | (0x20<<10)),  SRAI },
-  { (0b0000011 | (0x2<<7)),  LW },
-  { (0b0100011 | (0x2<<7)),  SW },
-  { (0b1100011 | (0x0<<7)),  BEQ },
-};
-
 Instruction
 decode(const instr_t _instr)
 {
-  /* TODO: */
-  /* - verify/test imm calculation */
+  // TODO: verify/test imm calculation
 
   uint8_t     opcode(0), rd(0), rs1(0), rs2(0), funct3(0), funct7(0);
   uint32_t    imm(0);
   mnemonic_t  mnemonic;
   format_t    format;
+
+  const unordered_map<uint8_t, format_t> format_map = {
+    { 0b0110011, R },
+    { 0b0010011, I },
+    { 0b1100011, B },
+    { 0b0000011, I },
+    { 0b0100011, S },
+  };
 
   opcode          = _instr & 127;
   format          = format_map.at(opcode);
@@ -103,9 +79,31 @@ decode(const instr_t _instr)
       break;
   }
 
+  const unordered_map<uint32_t, mnemonic_t> mnemonic_map {
+    // { (f7<<10 | f3<<7 | opcode), mnemonic }
+    // Opcode         f3         f7
+    { (0b0110011 | (0x0<<7) | (0x00<<10)),  ADD },
+    { (0b0110011 | (0x0<<7) | (0x20<<10)),  SUB },
+    { (0b0110011 | (0x4<<7) | (0x00<<10)),  XOR },
+    { (0b0110011 | (0x6<<7) | (0x00<<10)),  OR },
+    { (0b0110011 | (0x7<<7) | (0x00<<10)),  AND },
+    { (0b0110011 | (0x1<<7) | (0x00<<10)),  SLL },
+    { (0b0110011 | (0x5<<7) | (0x00<<10)),  SRL },
+    { (0b0110011 | (0x5<<7) | (0x20<<10)),  SRA },
+    { (0b0010011 | (0x0<<7)),               ADDI },
+    { (0b0010011 | (0x4<<7)),               XORI },
+    { (0b0010011 | (0x6<<7)),               ORI },
+    { (0b0010011 | (0x7<<7)),               ANDI },
+    { (0b0010011 | (0x1<<7) | (0x00<<10)),  SLLI },
+    { (0b0010011 | (0x5<<7) | (0x00<<10)),  SRLI },
+    { (0b0010011 | (0x5<<7) | (0x20<<10)),  SRAI },
+    { (0b0000011 | (0x2<<7)),               LW },
+    { (0b0100011 | (0x2<<7)),               SW },
+    { (0b1100011 | (0x0<<7)),               BEQ },
+  };
+
   mnemonic = mnemonic_map.at(opcode | (funct3<<7) | (funct7<<10));
 
   Instruction instr(opcode, rd, rs1, rs2, funct3, funct7, imm, mnemonic, format);
-
   return instr;
 }
