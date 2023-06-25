be5b4cb708f8a118f57dc20ed1d8fa337bab6fcc
Enan Ajmain <3nan.ajmain@gmail.com>
2023-06-25 23:51:41 +0600

    test: add function to test regfile equality

--- test/tests/test_mem.cc
+++ test/tests/test_mem.cc
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <cstring>
-#include <unordered_map>
-#include <vector>
 #include "util.h"
 #include "instruction.h"
 #include "decoder.h"
@@ -9,31 +7,14 @@
 #include "alu.h"
 #include "memory.h"
 #include "regfile.h"
+#include "test_util.h"
 
 using namespace std;
 
-int
-main(int argc, char **argv)
+void
+simulate(instr_t instrs[], int n)
 {
-  puts("This is RISC-V");
-
-  instr_t instrs[] = {
-    0x00100313,   // addi x6, x0, 1
-    0x01f31313,   // slli x6, x6, 31
-    0x00832383    // lw   x7, 8(x6)
-  };
-
-  // In reality, it is read from ELF (_start symbol)
-  ram_start = 0x80000000;
-  memset(ram, 0, RAM_SZ);
-
-  ram[8] = 0x10;
-
-  // TODO: Load instructions in ram
-
-  // TODO: set PC to ram_start?
-
-  for (auto _instr: instrs) {
+  for (int i = 0; i < n; ++i) {
     uint32_t        rd1, rd2;
     uint8_t         alu_ctrl;
     uint32_t        alu_out;
@@ -41,7 +22,7 @@ main(int argc, char **argv)
 
     // -- ID Stage ---------------------------------------------------------------
 
-    Instruction instr = decode(_instr);
+    Instruction instr = decode(instrs[i]);
     instr.print();
 
     Control ctrl(instr.opcode);
@@ -60,7 +41,7 @@ main(int argc, char **argv)
     if (ctrl.mem_read) {
       uint32_t addr = alu_out;
       if (read_ram(addr, &ram_data, instr.funct3)) {
-        puts("[ERR] main: read_ram failed");
+        puts("[ERR] read_ram failed");
       }
     } else {
       ram_data = 0;
@@ -78,6 +59,36 @@ main(int argc, char **argv)
     }
 
     dump_regs();
+  }
+}
+
+int
+main(int argc, char **argv)
+{
+  puts("This is RISC-V");
+
+  ram_start = 0x80000000;
+  memset(ram, 0, RAM_SZ);
+
+  // Test LW
+  {
+    instr_t instrs[] = {
+      0x00100313,   // addi x6, x0, 1
+      0x01f31313,   // slli x6, x6, 31
+      0x00832383    // lw   x7, 8(x6)
+    };
+
+    ram[8] = 0x10;
+    simulate(instrs, sizeof(instrs)/sizeof(instrs[0]));
+
+    reg_t test_regfile[] = {
+      0, 0, 0, 0, 0, 0, 0x80000000, 0x10,
+      0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0,
+    };
+
+    TEST(eq_regfile(test_regfile), "%s\n", "LW");
   }
 
   return 0;
